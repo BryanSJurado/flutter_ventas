6d5a0f30ac1ba9b789887d2e11b795594b69d61c
BryanSJurado <95387512+BryanSJurado@users.noreply.github.com>
2022-07-30 20:38:57 -0500

    commit

--- lib/src/Enviroment/environment.dart
+++ lib/src/Enviroment/environment.dart
@@ -1,4 +1,5 @@
 class Environment{
   static const String API_URL = "http://www.serviciosAMVentas.somee.com/servicios/";
   //static const String API_URL = "http://www.WebServiceVentas.somee.com/servicios/";
+
 }
